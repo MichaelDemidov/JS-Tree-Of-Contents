80d098f7666bfe2023055dbae14756a8d4e41b3c
Michael Demidov <michael.v.demidov@gmail.com>
2024-04-29 18:21:08 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# JSTreeOfContents
-This is a simple JavaScript class that creates a tree-like table of contents whose nodes can be collapsed and expanded
+Tree of Contents (HTML + JavaScript + CSS)
+==========================================
+
+What Is It?
+-----------
+
+This is a simple JavaScript class that creates a tree-like table of contents whose nodes can be collapsed and expanded. The part of the HTML document that does not belong to the selected node becomes invisible. The easiest way to illustrate this is with a picture.
+
+![A document without the table of contents](images/no_toc.jpg.png) ![The same document with the table of contents](images/toc.jpg.jpg)
+
+> [!NOTE]
+> The source code is provided as is, since I mainly did this project for myself, but I will be glad if someone finds it useful. Also, I physically could not test the script on a large number of browsers and especially mobile devices. Thus, I won’t mind if someone finds errors or wants to help the project in a different way.
+
+Requirements
+------------
+
+HTML 5, ECMAScript 6, and CSS 3.0. I also used scalable SVG icons that I drew myself for the tree elements (these are included in the CSS in Base64 encoded form and placed in the `images` folder as SVG images at the same time).
+
+I think it's possible to rewrite the code to reduce the requirements if someone needs it, but I haven't done it myself and haven't tested it on very old browsers like Internet Explorer.
+
+How It Works?
+-------------
+
+There is a `TreeOfContents` class that is instantiated in the document `onload` event handler. An element is passed to the class constructor contains text marked up using headings of different levels (from H1 to H6). The script uses them as “milestones” to divide the text into parts and simultaneously creates a tree of these headings. The tree is built from bulleted lists `<ul>` and collapsible HTML5 elements `<details>` and `<summary>` if needed. Each tree element contains a link to the corresponding part of the document, and all these parts are initially made invisible. The tree constructed in this way is placed either in an element passed to the `TreeOfContents` class constructor as the second parameter, or in an element `<div class="tree-of-contents">` that the class creates itself if the second constructor parameter is not spacified. Clicking on a link in the tree makes the document part visible (custom `<a>` tag attributes are used for this).
+
+The main difficulty is that the HTML document actually does not have any hierarchy connecting the headings of different levels with each other and with the content of the corresponding sections. The heading is not connected in any way to the text that follows it. Therefore, the script considers the section content to be everything placed between the heading and the following heading or the end of the text. If the next heading has a lower level than the current one, then the script considers that the next heading is nested inside the current section.
+
+To divide the document into parts, the script uses the `Range` class, which has a useful `surroundContents()` method. Without this class and its method, solving the problem would be hard because the script would have to cut out some of the content and paste it back.
+
+> [!CAUTION]
+> If the structure of the document is broken, that is, there are unclosed tags or headings are nested within other elements, the script will most likely produce strange results or not work at all.
+
+In any tree branch, the depth of a node's nesting is determined by the header level. The script is “smart” enough to handle situations where some header level does not exist, such as a document containing only H1, H2 and H4 headings: it treats the H4 heading as a third level rather than a fourth. Additionally, if one of the branch of the tree has a gap in the header numbering, the script inserts a “fake” tree node to maintain equal nesting levels compared to other branches.
+
+I tried to make the JS code, CSS, and HTML minimally dependent on each other. Of course, the script uses some CSS class names, but, firstly, there are not many of them, and secondly, all these names are placed at the very beginning of the script file as string constants, so they are easy to rename. And the only CSS class that is absolutely necessary is the class `hidden`, because it controls the visibility of elements (I also didn't use tricks like setting the `display` property to `none` in the script itself — I just assigned the objects the CSS class `hidden`). All other classes are intended for visual design and can easily be redefined as desired.
+
+A Little Bonus
+--------------
+
+This is itself part of a collection of quotes in various languages that I have been collecting since the mid-2000s.
+
+Author
+------
+Copyright (c) 2024, Michael Demidov
+
+Visit my GitHub page to check for updates, report issues, etc.: https://github.com/MichaelDemidov
+
+Drop me an e-mail at: michael.v.demidov@gmail.com
---
+++ tree-of-contents.js
@@ -0,0 +1,279 @@
+/** @type {string} Both CSS class for the table of contents and prefix for its id */
+const classTOC = "tree-of-contents";
+
+/** @type {string} Both the CSS class for the generated div, and the immutable ID part of all generated elements (used by TreeOfContents.processHashLink()) */
+const classTOCBlock = "toc-block";
+
+/** @type {string} CSS class for the invisible element (used by TreeOfContents.processHashLink()) */
+const classHidden = "hidden";
+
+/** @type {string} CSS class for the current selected link */
+const classCurrent = "current";
+
+/** @type {string} CSS class for the visited link */
+const classVisited = "visited";
+
+/** @type {string} Generated link id prefix */
+const prefixLink = "link-";
+
+/** @type {string} The name of a custom attribute that contains the id of the tree containing the generated link */
+const attrLinkTree = "tree-element";
+
+/** @type {string} The name of a custom attribute that contains the id of the section being referenced by the generated link */
+const attrLinkSection = "section";
+
+/** @type {integer} Global variable to ensure continuous numbering of all links in the document */
+let blockNumber = 0;
+
+/**
+ * Class that creates the tree-like table of contents
+ *
+ * @export
+ * @class TreeOfContents
+ */
+class TreeOfContents {
+  #elementContent;
+  #elementTreeOfContents;
+
+  /**
+   * Creates an instance of TreeOfContents
+   * 
+   * @param {Element} elementContent - Element containing content for which a table of contents is generated
+   * @param {Element} elementTreeOfContents - Parent element for the table of contents. If not specified, it will be created
+   * @memberof TreeOfContents
+   */
+  constructor(elementContent, elementTreeOfContents) {
+    this.#elementContent = elementContent;
+    if (elementTreeOfContents) {
+      this.#elementTreeOfContents = elementTreeOfContents;
+    } else {
+      this.#elementTreeOfContents = document.body.insertBefore(document.createElement("div"), this.#elementContent);
+    }
+    if (!this.#elementTreeOfContents.id) {
+      this.#elementTreeOfContents.id = classTOC + "-" + elementContent.id;
+    }
+    this.#elementTreeOfContents.classList.add(classTOC);
+    this.#initTree();
+  }
+
+  /**
+   * Expands a tree branch to display the selected link (see clickTreeItem())
+   * @param {Element} link - Current tree link element
+   * @memberof TreeOfContents
+   * @static
+   */
+  static expandTreeBranch(link) {
+    let itemToOpen = link.parentElement;
+    while (itemToOpen) {
+      if (itemToOpen.nodeName === "DETAILS") {
+        itemToOpen.setAttribute("open", "");
+      }
+      itemToOpen = itemToOpen.parentElement;
+    }
+  }
+
+  /**
+   * Onclick event listener of tree link element
+   *
+   * @memberof TreeOfContents
+   * @static
+   */
+  static clickTreeItem() {
+    // Attention! This is an event listener, so remember that "this" here is an <a> link, not a TreeOfContents object!
+    TreeOfContents.expandTreeBranch(this);
+
+    let div = document.getElementById(this.getAttribute(attrLinkSection));
+    let content = div.parentElement;
+    content.childNodes.forEach(current => {
+      if (current.nodeName === "DIV") {
+        current.classList.add(classHidden);
+      }
+    });
+    div.classList.remove(classHidden);
+    content.classList.remove(classHidden);
+
+    document.getElementById(this.getAttribute(attrLinkTree)).querySelectorAll("a").forEach(link => {
+      if (link === this) {
+        link.classList.add(classCurrent, classVisited);
+      } else {
+        link.classList.remove(classCurrent);
+      }
+    });
+  }
+
+  /**
+   * Find the element by the href, expand the tree branch that relates to it. This function is called either by
+   * the onclick event listener of internal link elements (i.e. <a href="#...">) or if the page URL contains such a link
+   *
+   * @param {string} href - Link href to process; either "#link" or "http://some.url#link"
+   * @param {boolean} [forceScroll=false] - If true, then force the browser to scroll the window to the desired element
+   * @return {boolean} - Always true to force the browser to scroll to target href element of the link
+   * @memberof TreeOfContents
+   * @static
+   */
+   static processHashLink(href, forceScroll = false) {
+    let targetElement = document.getElementById(href.split("#")[1]);
+    if (targetElement) {
+      let divToShow = targetElement.closest("." + classTOCBlock);
+      if (divToShow && divToShow.closest("." + classHidden)) {
+        let linkTOC = document.getElementById(prefixLink + divToShow.id);
+        if (linkTOC) {
+          linkTOC.click();
+        }
+        if (forceScroll) {
+          targetElement.scrollIntoView();
+        }
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Generates a tree of contents
+   *
+   * @memberof TreeOfContents
+   * @private
+   */
+  #initTree() {
+    if (this.#elementContent) {
+      let headerList = this.#elementContent.querySelectorAll("h1,h2,h3,h4,h5,h6");
+
+      if (headerList) {
+        this.#updateLinks(window.location.href.split("#")[0]);
+
+        /* variables */
+        let blockId = "";
+        let prevLevel = -1;
+        let currentLevel;
+        let ulRootTreeNode;
+        let ulCurrentTreeNode;
+        let liCurrentTreeNode;
+        let rangeCurrent;
+        let headerLevelList = this.#sortedNodeNameList(headerList);
+
+        headerList.forEach(headerCurrent => {
+          if (blockId !== "" && rangeCurrent) {
+            rangeCurrent.setEndBefore(headerCurrent);
+            this.#wrapRangeWithDiv(rangeCurrent, blockId).classList.add(classTOCBlock);
+          }
+
+          blockId = classTOCBlock + blockNumber++;
+          rangeCurrent = new Range();
+          rangeCurrent.setStartBefore(headerCurrent, 0);
+
+          for (currentLevel = headerLevelList.indexOf(headerCurrent.nodeName); currentLevel > prevLevel; prevLevel++) {
+            let tmp = document.createElement("ul");
+            if (prevLevel === -1 || typeof liCurrentTreeNode === "undefined") {
+              ulRootTreeNode = tmp;
+            } else {
+              let details = liCurrentTreeNode.appendChild(document.createElement("details"));
+              if (liCurrentTreeNode.childNodes[0].nodeName === "A") {
+                details.appendChild(document.createElement("summary")).appendChild(liCurrentTreeNode.childNodes[0]);
+              }
+              details.appendChild(tmp);
+            }
+            if (currentLevel > prevLevel + 1) {
+              liCurrentTreeNode = tmp.appendChild(document.createElement("li"));
+            }
+            ulCurrentTreeNode = tmp;
+          }
+
+          while (currentLevel < prevLevel) {
+            ulCurrentTreeNode = ulCurrentTreeNode.parentElement.closest("ul");
+            prevLevel--;
+          }
+          liCurrentTreeNode = ulCurrentTreeNode.appendChild(document.createElement("li"));
+          this.#createTreeLink(liCurrentTreeNode, headerCurrent.innerHTML, blockId)
+        });
+
+        if (blockId !== "" && rangeCurrent) {
+          let rangeLast = new Range;
+          rangeLast.selectNodeContents(this.#elementContent);
+          rangeLast.setStart(rangeCurrent.startContainer, rangeCurrent.startOffset);
+          this.#wrapRangeWithDiv(rangeLast, blockId);
+        }
+
+        this.#elementTreeOfContents.appendChild(ulRootTreeNode);
+
+        if (window.location.href.includes("#")) {
+          TreeOfContents.processHashLink(window.location.href, true);
+        }
+      }
+    }
+  }
+
+  /**
+   * Attaches the onclick event listener to tree link elements
+   *
+   * @param {string} strCurrentURL - If current URL contains the hash char "#"" then this is its prefix before the hash char
+   * @memberof TreeOfContents
+   * @private
+   */
+  #updateLinks(strCurrentURL) {
+    let linkList = document.querySelectorAll("a");
+    if (linkList) {
+      linkList.forEach(linkCurrent => {
+        let href = linkCurrent.href;
+        if (href.charAt(0) === "#" || strCurrentURL !== "" && href.indexOf(strCurrentURL) === 0) {
+          linkCurrent.addEventListener("click", (e) => {
+            return TreeOfContents.processHashLink(e.currentTarget.getAttribute("href"));
+          }, false);
+        }
+      });
+    }
+  }
+
+  /**
+   * Creates tree link
+   *
+   * @param {Element} parent - Parent of the link element
+   * @param {string} innerHTML - Content of the link
+   * @param {string} id - Id of the link
+   * @memberof TreeOfContents
+   * @private
+   */
+  #createTreeLink(parent, innerHTML, id) {
+    let link = parent.appendChild(document.createElement("a"));
+    /* TODO: direct assignment to innerHTML seems problematic because the inner HTML of the <h> element might
+       contain someting inappropriate for the <a>. I tried createTextNode(), but it doesn't works correctly if the
+       <h> contains style tags (<sub>, <strong>, etc.). I also tried the Range object, but this idea is even worse */
+    link.innerHTML = innerHTML;
+    link.id = prefixLink + id;
+    link.setAttribute(attrLinkSection, id);
+    link.setAttribute(attrLinkTree, this.#elementTreeOfContents.id);
+    link.setAttribute("href", "javascript://");
+    link.addEventListener("click", TreeOfContents.clickTreeItem);
+  }
+
+  /**
+   * Auxiliary function: surronds the Range with newly created <div> with given id
+   * 
+   * @param {Range} range - Range to surround
+   * @param {string} idNewDiv - New <div> identifier
+   * @return {Element} Newly created <div> itself
+   * @memberof TreeOfContents
+   * @private
+   */
+  #wrapRangeWithDiv(range, idNewDiv) {
+    let div = document.createElement("div");
+    div.id = idNewDiv;
+    range.surroundContents(div);
+    return div;
+  }
+
+  /**
+   * Auxiliary function: given a list of elements, returns an alphabetically sorted list of their unique node names
+   *
+   * @param {Array.<Element>} elementList - List of elements to process
+   * @return {Array.string} Alphabetically sorted list of node names
+   * @memberof TreeOfContents
+   * @private
+   */
+  #sortedNodeNameList(elementList) {
+    let elementSet = new Set();
+    elementList.forEach(child => {
+      elementSet.add(child.nodeName);
+    });
+    return Array.from(elementSet).sort();
+  }
+}
